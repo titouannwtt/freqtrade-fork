de2c1e1fa21856b7348b447afab2ec3e82e8224f
Matthias <xmatthias@outlook.com>
2025-12-06 13:42:16 +0100

    fix: floor timestamp to seconds
    no candle has more than second precision.

--- freqtrade/data/converter/converter.py
+++ freqtrade/data/converter/converter.py
@@ -38,7 +38,7 @@ def ohlcv_to_dataframe(
     cols = DEFAULT_DATAFRAME_COLUMNS
     df = DataFrame(ohlcv, columns=cols)
 
-    df["date"] = to_datetime(df["date"], unit="ms", utc=True)
+    df["date"] = to_datetime(df["date"], unit="ms", utc=True).dt.floor("s")
 
     # Some exchanges return int values for Volume and even for OHLC.
     # Convert them since TA-LIB indicators used in the strategy assume floats
