cba40c68a653728b7edc8a332db4c0c49e437f21
Ali Salama <alisalama@hotmail.co.uk>
2026-01-21 10:56:25 +0000

    Applied ruff formatting

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -2564,13 +2564,11 @@ class Exchange:
             )
         logger.debug(f"Downloaded data for {pair} from ccxt with length {len(data)}.")
         # funding_rates are always complete, so never need to be dropped.
-        drop = self._ohlcv_partial_candle if candle_type != CandleType.FUNDING_RATE else False
+        drop_incomplete = (
+            self._ohlcv_partial_candle if candle_type != CandleType.FUNDING_RATE else False
+        )
         return ohlcv_to_dataframe(
-            data,
-            timeframe,
-            pair,
-            fill_missing=False,
-            drop_incomplete=drop
+            data, timeframe, pair, fill_missing=False, drop_incomplete=drop_incomplete
         )
 
     async def _async_get_historic_ohlcv(
