917ee35277b8064c9758a8f0a21a49844dece218
Matthias <xmatthias@outlook.com>
2026-02-10 07:16:01 +0100

    refactor: switch SignalTypes to StrEnum

--- freqtrade/enums/signaltype.py
+++ freqtrade/enums/signaltype.py
@@ -1,7 +1,7 @@
-from enum import Enum, StrEnum
+from enum import StrEnum
 
 
-class SignalType(Enum):
+class SignalType(StrEnum):
     """
     Enum to distinguish between enter and exit signals
     """
@@ -11,20 +11,14 @@ class SignalType(Enum):
     ENTER_SHORT = "enter_short"
     EXIT_SHORT = "exit_short"
 
-    def __str__(self):
-        return f"{self.name.lower()}"
 
-
-class SignalTagType(Enum):
+class SignalTagType(StrEnum):
     """
     Enum for signal columns
     """
 
     ENTER_TAG = "enter_tag"
     EXIT_TAG = "exit_tag"
-
-    def __str__(self):
-        return f"{self.name.lower()}"
 
 
 class SignalDirection(StrEnum):
